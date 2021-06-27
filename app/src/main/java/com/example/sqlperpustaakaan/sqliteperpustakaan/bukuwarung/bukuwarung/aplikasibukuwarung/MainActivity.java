dd4ec25ab9a9a66ccf49a88360adc40bc03b84b8
FINALPROJEK <ganangsaputro38@gmail.com>
2021-06-27 20:27:56 +0700

    Menambahkan button dan textview

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.ap
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edusername,edpassword;
+    Button btmasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edusername = (EditText)findViewById(R.id.edusername);
+        edpassword = (EditText)findViewById(R.id.edpassword);
+
     }
 }
