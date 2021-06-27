eda57050810e9468f659b30742c3c81c3e33be05
FINALPROJEK <ganangsaputro38@gmail.com>
2021-06-27 21:03:46 +0700

    Menambahkan icon google

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.ap
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edusername,edpassword;
@@ -17,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
         edusername = (EditText)findViewById(R.id.edusername);
         edpassword = (EditText)findViewById(R.id.edpassword);
 
+    }
+
+    public void usergoogle(View view) {
+        Toast.makeText(this, "Selamat anda login", Toast.LENGTH_LONG).show();
+    }
+
+    public void userGmail(View view) {
+        Toast.makeText(this, "Selamat anda login", Toast.LENGTH_SHORT).show();
     }
 }
