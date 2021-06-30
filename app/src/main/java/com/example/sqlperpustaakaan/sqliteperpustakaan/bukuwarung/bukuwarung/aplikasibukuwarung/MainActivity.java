f43e785bad5d8bf97fbe980d3c7936b736f44b6b
FINALPROJEK <ganangsaputro38@gmail.com>
2021-06-30 07:59:05 +0700

    Memperbaiki intent, membuat menu transaksi,

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
@@ -2,30 +2,56 @@ package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.ap
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
-    EditText edusername,edpassword;
+    EditText edusername, edpassword;
     Button btmasuk;
 
+    private FirebaseAuth mAuth;
+    private String ContactId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edusername = (EditText)findViewById(R.id.edusername);
-        edpassword = (EditText)findViewById(R.id.edpassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        edusername = (EditText) findViewById(R.id.edusername);
+        edpassword = (EditText) findViewById(R.id.edpassword);
+        btmasuk=(Button)findViewById(R.id.btmasuk);
+        btmasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, menu_transaksi.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
-    public void usergoogle(View view) {
-        Toast.makeText(this, "Selamat anda login", Toast.LENGTH_LONG).show();
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
     }
 
     public void userGmail(View view) {
-        Toast.makeText(this, "Selamat anda login", Toast.LENGTH_SHORT).show();
+    }
+
+    public void usergoogle(View view) {
     }
 }
---
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/operasi.java
@@ -0,0 +1,10 @@
+package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.aplikasibukuwarung;
+
+import android.app.DownloadManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class operasi {
+
+
+}
