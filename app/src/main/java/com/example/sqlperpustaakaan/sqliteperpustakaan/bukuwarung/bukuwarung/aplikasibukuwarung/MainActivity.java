b101024a2a76d0b89e93e2d02c16ed6aa7b1ebe9
Sahas19SA1078 <sahasrestufaozi78@gmail.com>
2021-07-01 14:17:45 +0700

    Membuat menu login menggunakan register,menambahkan background pada menu login,membuat tampilan menu utama ,menambah button daftar harga barang,transaksi,catatan warung,hitungkeuntungan,menambah button keluar,menambah background pada tampilan menu utama , membuat tampilan menu daftar harga barang,

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/MainActivity.java
@@ -6,52 +6,83 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends AppCompatActivity {
-    EditText edusername, edpassword;
-    Button btmasuk;
+    DatabaseHelper db;
+    Button button1, button2, button3, button4, logout;
 
-    private FirebaseAuth mAuth;
-    private String ContactId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
 
-        edusername = (EditText) findViewById(R.id.edusername);
-        edpassword = (EditText) findViewById(R.id.edpassword);
-        btmasuk=(Button)findViewById(R.id.btmasuk);
-        btmasuk.setOnClickListener(new View.OnClickListener() {
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, menu_transaksi.class);
+                Intent intent = new Intent(MainActivity.this,DaftarHargaBarang.class);
                 startActivity(intent);
             }
         });
 
-    }
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,Transaksi.class);
+                startActivity(intent1);
+            }
+        });
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this,TambahHutang.class);
+                startActivity(intent2);
+            }
+        });
 
-    }
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainActivity.this,HitungKeuntungan.class);
+                startActivity(intent3);
+            }
+        });
 
-    private void updateUI(FirebaseUser currentUser) {
-    }
 
-    public void userGmail(View view) {
-    }
 
-    public void usergoogle(View view) {
+
+
+        db = new DatabaseHelper(this);
+
+        logout = (Button)findViewById(R.id.btn_logout);
+
+
+        Boolean checkSession = db.checkSession("ada");
+        if (checkSession == false) {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+
+        // logout
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean updtSession = db.upgradeSession("kosong", 1);
+                if (updtSession == true) {
+                    Toast.makeText(getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }
+        });
     }
 }
