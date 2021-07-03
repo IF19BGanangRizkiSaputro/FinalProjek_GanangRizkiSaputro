8f7742a231c55d670d7ad38d485d69f578061d88
FINALPROJEK <ganangsaputro38@gmail.com>
2021-07-03 09:25:04 +0700

    Membuat coding java transaksi

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/Transaksi.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/Transaksi.java
@@ -3,12 +3,67 @@ package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.ap
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Transaksi extends AppCompatActivity {
 
+    EditText etnamabarang,etjumlah,etdaftarharga,etbayar;
+    Button bttotal,bthasil,btsave;
+    TextView tv0,tvkembalian;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi);
+        etnamabarang=(EditText)findViewById(R.id.etnamabarang);
+        etjumlah=(EditText)findViewById(R.id.etjumlah);
+        etdaftarharga=(EditText)findViewById(R.id.etdaftarharga);
+        etbayar=(EditText)findViewById(R.id.etbayar);
+
+        bttotal=(Button) findViewById(R.id.bttotal);
+        bthasil=(Button) findViewById(R.id.bthasil);
+        btsave=(Button) findViewById(R.id.btsave);
+
+        tv0 =(TextView) findViewById(R.id.tv0);
+        tvkembalian=(TextView) findViewById(R.id.tvkembalian);
+
+        bttotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double jumlahbarang,hargabarang,total;
+                jumlahbarang=Double.valueOf(etjumlah.getText().toString().trim());
+                hargabarang=Double.valueOf(etdaftarharga.getText().toString().trim());
+                total=jumlahbarang*hargabarang;
+                String total1=String.valueOf(total);
+                tv0.setText(total1);
+
+            }
+        });
+        bthasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double bayar,totalharga,hasil;
+                totalharga=Double.valueOf(tv0.getText().toString().trim());
+                bayar=Double.valueOf(etbayar.getText().toString().trim());
+                hasil=bayar-totalharga;
+                String hasil1=String.valueOf(hasil);
+                tvkembalian.setText(hasil1);
+            }
+        });
+        btsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etnamabarang.setText("");
+                etdaftarharga.setText("");
+                etjumlah.setText("");
+                etbayar.setText("");
+                tv0.setText("");
+                tvkembalian.setText("");
+
+            }
+        });
+
     }
 }
