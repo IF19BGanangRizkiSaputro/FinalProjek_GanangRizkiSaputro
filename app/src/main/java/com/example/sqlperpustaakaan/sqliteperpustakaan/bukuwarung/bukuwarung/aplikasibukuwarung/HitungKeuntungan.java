1fe29e8f656fef03491afbd89b9e57cd3a0fa700
19SA1065-Andrian wibowo <andrianwibowo0@gmail.com>
2021-07-10 06:01:05 +0700

    Menambah menu Hitung keuntungan , membuat activity hitung keuntungan dan memperbaiki bug

--- app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/HitungKeuntungan.java
+++ app/src/main/java/com/example/sqlperpustaakaan/sqliteperpustakaan/bukuwarung/bukuwarung/aplikasibukuwarung/HitungKeuntungan.java
@@ -3,12 +3,39 @@ package com.example.sqlperpustaakaan.sqliteperpustakaan.bukuwarung.bukuwarung.ap
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HitungKeuntungan extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hitung_keuntungan);
+        EditText bil1, bil2;
+        TextView hasil;
+        Button kurang;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_hitung_keuntungan);
+
+            bil1 = (EditText) findViewById(R.id.bil1);
+            bil2 = (EditText) findViewById(R.id.bil2);
+            kurang = (Button) findViewById(R.id.kurang);
+            hasil = (TextView) findViewById(R.id.hasil);
+
+            kurang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Double a = Double.parseDouble(bil1.getText().toString());
+                    Double b = Double.parseDouble(bil2.getText().toString());
+                    Double result = a - b;
+                    hasil.setText(Double.toString(result));
+                }
+            });
+
+
+        }
     }
-}
+
+
